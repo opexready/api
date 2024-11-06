706f10b3fb7dbb872db34ffc15e3d3937f9e71d6
Sunat2020 <erosalesq.analista.c>
2024-11-06 10:12:23 -0500

    v1.10

--- app/schemas.py
+++ app/schemas.py
@@ -91,8 +91,55 @@ class Documento(DocumentoBase):
         from_attributes = True
 
 class DocumentoUpdate(BaseModel):
-    estado: Optional[str] = None
+    id: Optional[int] = None
+    fecha_solicitud: Optional[date] = None
     fecha_rendicion: Optional[date] = None
+    dni: Optional[str] = None
+    usuario: Optional[str] = None
+    gerencia: Optional[str] = None
+    ruc: Optional[str] = None
+    proveedor: Optional[str] = None
+    fecha_emision: Optional[date] = None
+    moneda: Optional[str] = None
+    tipo_documento: Optional[str] = None
+    serie: Optional[str] = None
+    correlativo: Optional[str] = None
+    tipo_gasto: Optional[str] = None
+    sub_total: Optional[float] = None
+    igv: Optional[float] = None
+    no_gravadas: Optional[float] = None
+    importe_facturado: Optional[float] = None
+    tc: Optional[float] = None
+    anticipo: Optional[float] = None
+    total: Optional[float] = None
+    pago: Optional[float] = None
+    detalle: Optional[str] = None
+    estado: Optional[str] = None
+    empresa: Optional[str] = None
+    archivo: Optional[str] = None
+    tipo_solicitud: Optional[str] = None
+    tipo_cambio: Optional[float] = None
+    afecto: Optional[float] = None
+    inafecto: Optional[float] = None
+    rubro: Optional[str] = None
+    cuenta_contable: Optional[int] = None
+
+    # Nuevos campos añadidos
+    responsable: Optional[str] = None
+    area: Optional[str] = None
+    ceco: Optional[str] = None
+    tipo_anticipo: Optional[str] = None
+    motivo: Optional[str] = None
+    fecha_viaje: Optional[date] = None
+    dias: Optional[int] = None
+    presupuesto: Optional[float] = None
+    banco: Optional[str] = None
+    numero_cuenta: Optional[str] = None
+    destino: Optional[str] = None
+    origen: Optional[str] = None
+    numero_rendicion: Optional[str] = None
+    tipo_viaje: Optional[str] = None
+
 
 class CompanyBase(BaseModel):
     name: str
