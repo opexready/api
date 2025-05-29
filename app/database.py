77049c6ee9c7e95c72b66b6a4fa09efffe11b525
Sunat2020 <erosalesq.analista.c>
2025-05-28 23:29:02 -0500

    sssss

--- app/database.py
+++ app/database.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 # Usando la URL pública para la conexión
 #DATABASE_URL_ASYNC = "postgresql+asyncpg://postgres:TaAfBftyWBvbTabbIzzvFOPvToXmszym@autorack.proxy.rlwy.net:29750/railway"
 #DATABASE_URL_ASYNC = "postgresql+asyncpg://postgres:LX125Uuisd@localhost:5432/bdRendicion"
-DATABASE_URL_ASYNC = "postgresql+asyncpg://postgres:bbtpqLmIwPywDPsPyBkyKlnKwofAoIcD@ballast.proxy.rlwy.net:28910/railway"
+DATABASE_URL_ASYNC = "postgresql+asyncpg://postgres:YymcJjDqeEuRMZYuIKyriwMKeKFiIGjA@yamanote.proxy.rlwy.net:37873/railway"
 # Crear el motor y la sesión de la base de datos
 engine = create_async_engine(DATABASE_URL_ASYNC)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, class_=AsyncSession)
